9b5fbd966cb33fd9e9911900c8d116f05f9f26a7
DESKTOP-65SAUST\Alumno <pelicesr@gmail.com>
2024-11-13 14:32:13 +0100

    Update slideshow.js

--- JS/slideshow.js
+++ JS/slideshow.js
@@ -1,1 +1,12 @@
-console.log("online!")
+/**
+ * Funciones en el scripy slideshow.js
+ * 
+ * estas van a controlar el funcionamiento del componente slideshow
+ * 
+ * este slideshow tendra unos controladores para pasar hacia alante y hacia atras
+ * los slides
+ * 
+ * tambien tendra unos puntos para poder seleccionar y ver cada slide
+ * 
+ * tambien sera automatico y podremos ir viendo cada slide cada cierto tiempo.
+ */
