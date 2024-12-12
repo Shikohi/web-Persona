d4935031ec3318d11b99cd8d2e5f8b90c31de707
mimieow <angy2004lol@hotmail.com>
2024-12-12 19:13:31 +0100

    Formulario funcional

--- JS/slideshow.js
+++ JS/slideshow.js
@@ -1,15 +1,3 @@
-/**
- * Funciones en el scripy slideshow.js
- * 
- * estas van a controlar el funcionamiento del componente slideshow
- * 
- * este slideshow tendra unos controladores para pasar hacia alante y hacia atras
- * los slides
- * 
- * tambien tendra unos puntos para poder seleccionar y ver cada slide
- * 
- * tambien sera automatico y podremos ir viendo cada slide cada cierto tiempo.
- */
 
 //inicializamos un contador slideIndex
 let slideIndex = 1;
@@ -150,5 +138,7 @@ menuLinks.forEach(link => {
         slideshow.classList.remove("hidden");
     });
 });
+
+
 
 
